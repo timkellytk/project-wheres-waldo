273e6b467cafe37cd08f8468bf27939c912535ea
Tim Kelly <timpkelly1@gmail.com>
2020-11-26 15:22:45 +1100

    Added an image to the game and created a formulat to getLocationImageClick

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import GameWrapper from '../components/GameWrapper/GameWrapper';
+import Level1 from '../img/levels/level-1.jpg';
 
-const Game = () => (
-  <GameWrapper>
-    <h1>hello</h1>
-  </GameWrapper>
-);
+const Game = () => {
+  const getLocationImageClick = (e) => {
+    const xCoord = e.nativeEvent.offsetX / e.nativeEvent.target.offsetWidth;
+    const yCoord = e.nativeEvent.offsetY / e.nativeEvent.target.offsetHeight;
+    const coords = { xCoord, yCoord };
+    return coords;
+  };
+
+  return (
+    <GameWrapper>
+      <img
+        className="w-full h-full"
+        src={Level1}
+        alt="level 1"
+        onClick={(e) => console.log(getLocationImageClick(e))}
+      />
+    </GameWrapper>
+  );
+};
 
 export default Game;
