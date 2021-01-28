44ac168d8bedfbc3342c346e6dad2e095cf66812
Tim Kelly <tim.kelly@sca.com.au>
2021-01-28 14:53:46 +1100

    Succesfully check if the character exists in the level

--- functions/handlePlayerSelection.js
+++ functions/handlePlayerSelection.js
@@ -1,11 +1,39 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const { LEVELS } = require("./helpers/constants/index");
+
+const db = admin.firestore();
 
 const handlePlayerSelection = functions.firestore
-    .document('playerSelection/{docId}')
-    .onCreate((snap, context) => {
-        const { character, coords, gameId, level } = snap.data();
-        console.log(character, coords, gameId, level);
-        return true;
-    })
+  .document("playerSelection/{docId}")
+  .onCreate(async (snap, context) => {
+    const { character, coords, gameId, level } = snap.data();
+
+    const levelSnap = await db
+      .collection(LEVELS)
+      .where("level", "==", level)
+      .get();
+    const levelData = levelSnap.docs
+      .map((doc) => {
+        return doc.data();
+      })
+      .reduce((doc) => doc);
+    const isCharacterAtCoords = levelData.characters.some(
+      (char) =>
+        char.name === character &&
+        char.xCoord === coords.xCoord &&
+        char.yCoord === coords.yCoord
+    );
+
+    console.log(
+      character,
+      coords,
+      gameId,
+      level,
+      levelData,
+      isCharacterAtCoords
+    );
+    return true;
+  });
 
 module.exports = handlePlayerSelection;
