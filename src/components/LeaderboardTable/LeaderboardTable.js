41daca62bbe997128b717ee73d7df49c4edade66
Tim Kelly <tim.kelly@sca.com.au>
2021-02-01 16:15:19 +1100

    Added the unit of time measurement

--- src/components/LeaderboardTable/LeaderboardTable.js
+++ src/components/LeaderboardTable/LeaderboardTable.js
@@ -30,7 +30,7 @@ const LeaderboardTable = (props) => {
                   scope="col"
                   className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                 >
-                  Time
+                  Time (Seconds)
                 </th>
               </tr>
             </thead>
